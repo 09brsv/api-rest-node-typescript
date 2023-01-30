c1a2f153c46824d760476c83ca5af82f30111b0c
Bruno <brunoornelio@hotmail.com>
2023-01-30 14:58:24 -0300

    Config knex, sqlite e migrations

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
+import { ICidade } from "../../database/models";
 
 import { validation } from "../../shared/middleware";
 
-interface ICity {
-  nome: string;
+interface IBodyProps extends Omit<ICidade, 'id'> {
 }
 
 export const createValidation = validation((getSchema) => ({
-  body: getSchema<ICity>(yup.object().shape({
+  body: getSchema<IBodyProps>(yup.object().shape({
     nome: yup.string().strict(true).required().min(3)
   }))
 }));
 
-export const create = async (req: Request<{}, {}, ICity>, res: Response) => { 
+export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
   res.status(StatusCodes.CREATED).json(1)
 };
