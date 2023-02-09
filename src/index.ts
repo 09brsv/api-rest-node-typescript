1e9217cc66a913aec54f7b946e5a37a7139869e2
Bruno <brunoornelio@hotmail.com>
2023-02-09 14:12:23 -0300

    fix: change environment

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,20 @@ import { Knex } from "./server/database/knex";
 import { server } from "./server/Server";
 
 const startServer = () => {
+  server.listen(process.env.PORT || 3333, () =>
+    console.log(
+      `Servidor inicializado em http://localhost:${process.env.PORT || 3333}`
+    )
+  );
+};
 
-  server.listen(process.env.PORT || 3333, () => console.log(`Servidor inicializado em http://localhost:${process.env.PORT || 3333}`))
-}
-
-if (process.env.IS_LOCAL_SERVER !== "true") {
-  
-  Knex.migrate.latest().then(() => {
-    startServer();
-  }).catch(console.log)
-}else{
+if (process.env.IS_LOCALHOST !== "true") {
+  Knex.migrate
+    .latest()
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log);
+} else {
   startServer();
 }
