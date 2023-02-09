99669a1e83c13729ccba1b2486e3f98c1337d890
Bruno <brunoornelio@hotmail.com>
2023-02-09 10:06:35 -0300

    fix: change dependencies, active migrations in production

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/Server";
 
-server.listen(process.env.PORT || 3333, () => console.log(`Servidor inicializado em http://localhost:${process.env.PORT || 3333}`))
+const startServer = () => {
+
+  server.listen(process.env.PORT || 3333, () => console.log(`Servidor inicializado em http://localhost:${process.env.PORT || 3333}`))
+}
+
+if (process.env.IS_LOCAL_SERVER !== "true") {
+  
+  Knex.migrate.latest().then(() => {
+    startServer();
+  }).catch(console.log)
+}else{
+  startServer();
+}
